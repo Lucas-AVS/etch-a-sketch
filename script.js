4fe91a32a8fb7b635fcd736d6ae2d86be3934131
Ponk8 <lucas.alencar31@gmail.com>
2023-12-04 17:16:58 -0300

    Refactor: Replace deprecated 'event.path' with 'event.target'

--- script.js
+++ script.js
@@ -5,67 +5,66 @@ let eraseButton = document.querySelector('.erase');
 let currentColor = 'black';
 
 function getGrid(n) {
-    for(i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         let div = document.createElement('div');
-        // div.dataset.indexNumber = i;
         div.classList.add('grid-item');
         container.appendChild(div);
     }
 }
 
-//grid number is a number thats equal height * weight. Ex: (32 * 32 = 1024) (16 * 16 = 256)...
 getGrid(1024);
 
-let startDrawing = (e) => {
-    if(e.path[0].classList.contains('grid-item')) {e.path[0].style.backgroundColor = currentColor;};
-};
-
-addEventListener('mousedown', function(e){
-    if(e.path[0].classList.contains('grid-item')) {
-        addEventListener('mouseup', function(){
-            removeEventListener('mouseover', startDrawing);
-        });
-        addEventListener('mouseover', startDrawing);   
+function startDrawing(e) {
+    if (e.target.classList.contains('grid-item')) {
+        e.target.style.backgroundColor = currentColor;
     }
-}) 
+}
 
-clearButton.addEventListener('click',
-function() {
-        let allGridItems = document.querySelectorAll('.grid-item');
-        allGridItems.forEach(div => {
-            div.style.backgroundColor = "white";
-        });
+function handleMouseUp() {
+    removeEventListener('mouseover', startDrawing);
+}
+
+function handleMouseDown(e) {
+    if (e.target.classList.contains('grid-item')) {
+        addEventListener('mouseup', handleMouseUp);
+        addEventListener('mouseover', startDrawing);
     }
-);
-colorButton.addEventListener('click', 
-function() {
+}
+
+addEventListener('mousedown', handleMouseDown);
+
+clearButton.addEventListener('click', function () {
+    let allGridItems = document.querySelectorAll('.grid-item');
+    allGridItems.forEach((div) => {
+        div.style.backgroundColor = 'white';
+    });
+});
+
+colorButton.addEventListener('click', function () {
     currentColor = colorPicker.value;
-    colorButton.setAttribute('id','selected');
-    eraseButton.removeAttribute('id','selected');
-    }
-);
+    colorButton.setAttribute('id', 'selected');
+    eraseButton.removeAttribute('id', 'selected');
+});
 
-eraseButton.addEventListener('click',
-function() {
-        currentColor = 'white';
-        eraseButton.setAttribute('id','selected');
-        colorButton.removeAttribute('id','selected');
-    }
-);
+eraseButton.addEventListener('click', function () {
+    currentColor = 'white';
+    eraseButton.setAttribute('id', 'selected');
+    colorButton.removeAttribute('id', 'selected');
+});
 
 // Color picker
 let colorPicker;
-const defaultColor = "#383737";
+const defaultColor = '#383737';
 
-window.addEventListener("load", startup, false);
+window.addEventListener('load', startup, false);
 
 function startup() {
     colorPicker = document.querySelector('.colorPicker');
     colorPicker.value = defaultColor;
-    colorPicker.addEventListener("input", updateFirst, false);
-  }
+    colorPicker.addEventListener('input', updateFirst, false);
+}
 
 function updateFirst(event) {
     currentColor = event.target.value;
     currentColor = colorPicker.value;
-};
+}
